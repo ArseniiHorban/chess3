b08b0ff978fb889793290c9fa137b6b9e2418eb3
exicap <arsenijtisenko1@gmail.com>
2024-11-27 07:45:58 +0200

    better DFS, still barely working UCS and better position evaluation

--- chess_bot_bfs.py
+++ chess_bot_bfs.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import chess
 from collections import deque
 from position_evaluator import PositionEvaluator
@@ -27,4 +28,35 @@ class ChessBotBFS:
                 queue.append((current_board.copy(), depth + 1, move if depth == 0 else first_move))
                 current_board.pop()
 
+=======
+import chess
+from collections import deque
+
+class ChessBotBFS:
+    def __init__(self, depth, evaluator):
+        self.depth = depth
+        self.evaluator = evaluator
+        self.nodes_explored = 0
+
+    def get_best_move(self, board):
+        best_move = None
+        best_value = float('-inf')
+        queue = deque([(board.copy(), 0, None)])
+
+        while queue:
+            current_board, depth, first_move = queue.popleft()
+
+            if depth >= self.depth or current_board.is_game_over():
+                value = self.evaluator.evaluate(current_board)
+                if value > best_value:
+                    best_value = value
+                    best_move = first_move
+                continue
+
+            for move in current_board.legal_moves:
+                current_board.push(move)
+                queue.append((current_board.copy(), depth + 1, move if depth == 0 else first_move))
+                current_board.pop()
+
+>>>>>>> 01c5b21 (better DFS, still barely working UCS and better position evaluation)
         return best_move
